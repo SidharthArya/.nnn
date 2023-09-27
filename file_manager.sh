5b80330982a808231fc9746ded7c65252c0cfe3b
Siharth Arya <code@sidhartharya.com>
2023-09-27 06:39:24 +0530

    Auto Commit: Wed Sep 27 06:39:24 AM IST 2023

--- file_manager.sh
+++ file_manager.sh
@@ -3,7 +3,8 @@ export NNN_BMS='D:~/Documents;l:/home/arya/Private/Lectures/;d:~/Downloads/;m:/r
 export NNN_SSHFS="sshfs -o follow_symlinks"        # make sshfs follow symlinks on the remote
 export NNN_COLORS="21365798"                           # use a different color for each context
 export NNN_TRASH=1                                 # trash (needs trash-cli) instead of delete
-export EDITOR=~/.config/nnn/actions/editor
-export NNN_PLUG='m:mount;p:preview-tabbed;s:subtitles.sh;d:dragdrop;e:edit'
+export EDITOR=nvim
+export EDITOR1=~/.config/nnn/actions/editor
+export NNN_PLUG='m:mount;p:preview-tabbed;s:subtitles.sh;d:dragdrop;e:edit;z:z;f:fzcd'
 export NNN_FIFO=/tmp/nnn.fifo
 #nnn -x
